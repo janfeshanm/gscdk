cbcc0092e52519b144d6a6a12c5ff68e36248da6
Jonathan Soufi <jsoufi.au@gmail.com>
2025-09-27 22:18:31 +1000

    Nest DevOps Cycle

--- lib/gscdk-stack.ts
+++ lib/gscdk-stack.ts
@@ -1,9 +1,43 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { IEcsApplicationConstruct, EcsApplicationConstruct } from './constructs/ecsApplication.construct';
+import { Repository } from 'aws-cdk-lib/aws-ecr';
 
 export class GscdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+const ecsApplicationConstructProps :IEcsApplicationConstruct ={
+            account:this.account,
+            region:this.region,
+            ecrConfig:{
+                name:"nestjs-app-sample",
+                id:"nestjs-app-sample",
+            },
+            ecsConfig:{
+                clusterName:"SampleCluster",
+                executionRole:{
+                    name:"fargate-test-task-execution-role",
+                    id:"fargate-test-task-execution-role"
+                },
+                taskDefinitionId:"sample-task-id",
+                containerConfig:{
+                    id:"sample-task-container",
+                    name:"sample-task-container"
+                },
+            },
+            pipelineConfig:{
+                    pipelineId:"nestJsBuildingApp",
+                    pipelineName:"nestJsBuildingApp",
+                    githubConfig:{
+                        owner:"janfeshanm",
+                        repo:"gsben",
+                        oAuthSecretManagerName:"GitHubToken",
+                        branch:"main"
+                    },
+                    buildSpecLocation:"./buildspec.yml"
+                }
+            }
 
+        const ecsApplicationConstruct: EcsApplicationConstruct = new EcsApplicationConstruct(this,"ecsApplicationConstruct",ecsApplicationConstructProps)
   }
 }
